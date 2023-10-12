a8bde7492cb1dd89f137e89914d3d98e3578f219
Jonathan Buzzetti <jonathan.buzzetti@naranjax.com>
2023-10-12 09:26:22 -0300

    actualizaciones 2023

--- basic/myscript.js
+++ basic/myscript.js
@@ -1,15 +1,19 @@
-// function myFunction(){
-//     alert("Hola")
+// function myFunction(persona) {
+//     alert("Hola " + persona.nombre)
 // }
+//
+// var per = {"nombre":"Jhon", "id":2}
+//
+// myFunction(per)
 
 
-//fetch es un GET por default
-fetch('http://localhost:8080/mascotas')
-    .then(response => {
-        if (response.status >= 400) {
+// fetch es un GET por default y OJO que es asincronico
+fetch('http://localhost:8080/api/mascotas')
+    .then(responseHttp => {
+        if (responseHttp.status >= 400) {
             alert("Hubo un error en el API")
         } else {
-            return response.json()
+            return responseHttp.json()
         }
     })
     .then(listaDeMascotas => {
@@ -18,21 +22,23 @@ fetch('http://localhost:8080/mascotas')
         console.log(a)
     })
 
-var a = 5 + 6
+
+var a = 5 + 6 + "A"
 console.log(a)
 
 
 function crearMascota() {
     var nuevaMascota = {
-        nombre: "Lala", raza: "PERRO"
+        "nombre": "Lala",
+        "raza": "PERRO"
     }
 
-    fetch("http://localhost:8080/mascotas", {
+    fetch("http://localhost:8080/api/mascotas", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(nuevaMascota)
+        body: JSON.stringify(nuevaMascota) //serializa objeto o array a JSON
     })
         .then(response => {
             if (response.status >= 400) {
@@ -42,16 +48,19 @@ function crearMascota() {
             }
         })
         .then(mascotaCreada => {
-            alert("Mascota creada: " + mascotaCreada.nombre)
-            document.getElementById("app").innerHTML += "<h3>" + mascotaCreada.nombre + "</h3>"
+            if(mascotaCreada != undefined) {
+                alert("Mascota creada: " + mascotaCreada.nombre)
+                document.getElementById("app").innerHTML += "<h3>" + mascotaCreada.nombre + "</h3>"
+            }
         })
 }
 
-
-fetch('http://localhost:8080/mascotas')
+fetch('http://localhost:8080/api/mascotas')
     .then(response => response.json())
     .then(mascotas => {
         document.getElementById("app").innerHTML += '<table id="tablita" class="table table-striped table-bordered"></table>'
-        mascotas.forEach(mascota => document.getElementById("tablita").innerHTML += "<tr><td>" + mascota.nombre + "</td></tr>")
+        mascotas
+            .forEach(mascota => document.getElementById("tablita").innerHTML +=
+                "<tr><td>" + mascota.nombre + "</td></tr>")
     })
 
